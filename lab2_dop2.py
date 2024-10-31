f3815a6a1bda5d0e8b68c6f85fa5ebcd4b40f3b3
toklee <t.kolesnikova06@mail.ru>
2024-10-31 14:42:18 +0500

    Update lab2_dop2.py

--- lab2_dop2.py
+++ lab2_dop2.py
@@ -1,13 +1,16 @@
 with open('books-en.csv') as a:
     list_books = a.readlines()
+    
 tabl = []
 
 for i in range(len(list_books)):
     tabl.append(list_books[i].split(';'))
+    
 popular = []
 
 for num in range(1,len(tabl)):
     popular.append(int(tabl[num][-2]))
+    
 MAXPOP = max(popular)
 count = 0
 
