56cae496add52e2f9d3bc0610dea235fa74d6e0f
toklee <t.kolesnikova06@mail.ru>
2024-10-31 14:48:52 +0500

    Update lab2_dop2.py

--- lab2_dop2.py
+++ lab2_dop2.py
@@ -8,14 +8,14 @@ for i in range(len(list_books)):
     
 popular = []
 
-for num in range(1,len(tabl)):
+for num in range(1, len(tabl)):
     popular.append(int(tabl[num][-2]))
     
 MAXPOP = max(popular)
 count = 0
 
-for title in range(1,len(tabl)):
-    if count<20 and int(tabl[title][-2]) == MAXPOP:
+for title in range(1, len(tabl)):
+    if count < 20 and int(tabl[title][-2]) == MAXPOP:
         print(tabl[title][1])
-        count+=1
+        count += 1
 
