1d8566eee818c5095e6aeb47120b514ef7c96feb
toklee <t.kolesnikova06@mail.ru>
2024-10-31 14:41:16 +0500

    Update lab2_1.py

--- lab2_1.py
+++ lab2_1.py
@@ -5,7 +5,8 @@ tabl = []
 for i in range(len(list_books)):
     tabl.append(list_books[i].split(';'))
 #print(tabl[0][1])
-count=0
+count = 0
+
 for number in range(len(tabl)):
     if len(tabl[number][1]) > 30:
         count += 1
